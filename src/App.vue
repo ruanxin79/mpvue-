a0f73d6addf4ac92fe288afec5156ec4ce625ea2
张昊旻 <zhanghmc@sunits.com>
2018-05-15 16:01:45 +0800

    add iconfont library

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+import './common/fontello/css/fontello.css'
 export default {
   created () {
     // 调用API从本地缓存中获取数据
@@ -6,19 +7,10 @@ export default {
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    console.log('app created and cache logs by setStorageSync')
   }
 }
 </script>
 
 <style>
-    .container {
-    /*  height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    padding: 200rpx 0;
-    box-sizing: border-box;*/
-    }
+
 </style>
